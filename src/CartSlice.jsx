488a40746774465f6eb9310da7ee156ae9acd471
Julio Guadarrama <julio.guadarrama10@gmail.com>
2026-09-03 14:31:18 -0400

    Fix reducers and update CartSlice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -3,10 +3,9 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [], // Inicializa el array de productos en el carrito
+    items: [],
   },
   reducers: {
-    // 1. Agregar un artículo al carrito
     addItem: (state, action) => {
       const { name, image, cost } = action.payload;
       const existingItem = state.items.find(item => item.name === name);
@@ -16,12 +15,10 @@ export const CartSlice = createSlice({
         state.items.push({ name, image, cost, quantity: 1 });
       }
     },
-    // 2. Eliminar un artículo del carrito
     removeItem: (state, action) => {
       const { name } = action.payload;
       state.items = state.items.filter(item => item.name !== name);
     },
-    // 3. Actualizar la cantidad de un artículo específico
     updateQuantity: (state, action) => {
       const { name, quantity } = action.payload;
       const itemToUpdate = state.items.find(item => item.name === name);
